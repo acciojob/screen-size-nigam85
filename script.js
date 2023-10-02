e564bef21506d3e555f89636f7c5754a202f652b
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-02 14:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+  const widthElement = document.getElementById('width');
+        const heightElement = document.getElementById('height');
+
+        // Function to update the width and height elements
+        function updateSize() {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            widthElement.textContent = width;
+            heightElement.textContent = height;
+        }
+
+        // Initial call to set the initial size
+        updateSize();
+
+        // Add an event listener to the window's resize event
+        window.addEventListener('resize', updateSize);
